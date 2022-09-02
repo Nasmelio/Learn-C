b1c6e9aec223cb930ebe16b3145c43fd44e0a2f6
Nasmelio <92834344+Nasmelio@users.noreply.github.com>
2022-09-02 22:32:58 +0800

    Add files via upload

---
+++ day9.c
@@ -0,0 +1,118 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("%d %d %d", a, b, c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = -128;
+//	printf("%u", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = -10;
+//	unsigned int n = 20;
+//	printf("%d", i + n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a[100];
+//	int i;
+//	for (i = 0; i < 100; i++)
+//	{
+//		a[i] = -1 - i;
+//		printf("%d\n", a[i]);
+//	}
+//	printf("\n%d", strlen(a));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	signed char i = 0;
+//	for (i = 0; i <= 126; i++)
+//	{
+//		printf("hello world %d\n",i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	float a = 5.5;
+//	printf("%0.1f", a);
+//	return 0;
+//}
+
+//void test(int arr[])
+//{
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", sz);
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	test(arr);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "abscd";
+//	char* c=arr;
+//	printf("%s\n", arr);
+//	printf("%s\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "abcdf";
+//	char arr2[] = "abcdf";
+//	char* p1 = "abcdf";
+//	char* p2 = "abcdf";
+//	if (arr1 == arr2)
+//	{
+//		printf("hehe\n");
+//
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//
+//	if (p1 == p2)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//
+//}
